ea60423ac2cafd7b556d64dff60d6046b9673a1f
Hb_W <hugobarillasvi@hotmail.com>
2024-02-08 09:31:58 -0600

    Creación de la clase GnomeSort
    Aplicación del gnome sort para la hoja de trabajo

---
+++ src/main/java/uvg/edu/gt/GnomeSort.java
@@ -0,0 +1,20 @@
+package uvg.edu.gt;
+
+public class GnomeSort {
+    public static void sort(int[] arr) {
+        int index = 0;
+        while (index < arr.length) {
+            if (index == 0) {
+                index++;
+            }
+            if (arr[index] >= arr[index - 1]) {
+                index++;
+            } else {
+                int temp = arr[index];
+                arr[index] = arr[index - 1];
+                arr[index - 1] = temp;
+                index--;
+            }
+        }
+    }
+}
